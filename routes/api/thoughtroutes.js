d44c6ec93a07065d45aad308c424cf8d779679e6
Nate Long <nate.k.long@gmail.com>
2023-05-11 14:33:18 -0400

    user routes + comments

--- routes/api/thoughtroutes.js
+++ routes/api/thoughtroutes.js
@@ -11,12 +11,16 @@ const {
 
 } = require('../../controllers/thoughtController');
 
+// /api/thoughts
 router.route('/').get(getAllThoughts).post(createNewThought);
 
+// /api/thoughts/:Id
 router.route('/:Id').get(getThoughtById).put(updateThought).delete(deleteThought);
 
+// /api/thoughts/:thoughtId/reactions
 router.route('/:thoughtId/reactions').post(createReaction);
 
+// /api/thoughts/:thoughtId/reactions/:reactionId
 router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
 
 module.exports = router;
--- routes/api/userroutes.js
+++ routes/api/userroutes.js
@@ -2,13 +2,22 @@ const router = require('express').Router();
 const {
   getUsers,
   getUserById,
-  createUser,
+  createNewUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  deleteFriend
+
 } = require('../../controllers/userController');
 
 // /api/users
-router.route('/').get(getUsers).post(createUser);
+router.route('/').get(getUsers).post(createNewUser);
 
 // /api/users/:userId
-router.route('/:userId').get(getUserById);
+router.route('/:userId').get(getUserById).put(updateUser).delete(deleteUser);
+
+// /api/users/:userId/friends/:friendId
+router.route('/:userId/friends/:friendId').post(addFriend).delete(deleteFriend);
+
 
 module.exports = router;
